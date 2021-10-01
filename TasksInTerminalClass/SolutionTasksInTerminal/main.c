d278a60a727e42596f5dc206509936ca2b3556ae
itmm-2112-a <itmm-2112-a@P202101003>
2021-10-01 08:56:39 +0300

    class work

--- TasksInTerminalClass/SolutionTasksInTerminal/main.c
+++ TasksInTerminalClass/SolutionTasksInTerminal/main.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 
-/*
-*/
-
-
 int main() {
 	int number;
-	int isWork;
-	/*
+	int isWork = 1;
+
+	while (isWork) {
+		printf("Let's ivers numbers!\n");
+		printf(" For exit enter 0\n");
+		/* ... */
+
+		int isError = 0;
+
+		//GOOD CODE
+		do {
+			if (isError) printf("Wrong Input: Try again\n");
+
+			printf("Input positive number: ");
+			scanf_s("%d", &number);
+
+			if (number == 0) {
+				isWork = 0; // work = lie
+				break;
+			}
+
+			isError = (number <= 0);
+		} while (isError);
+
+		int invers = 0;
+
+		// ...
+		printf("Result: %d", invers);
+	}
+
+  return 0;
+}
+/*
 	printf("Let's ivers numbers!\n");
 
 	do {
 	printf("Input number: ");
 	scanf_s("%d", &number);
 	} while (number < 0);
-	
+
 	return 0;
 }
 */
-
-	int isError = 0;
-
-  //GOOD CODE
-	do {
-		if (isError) printf("Wrong Input: Try again\n");
-
-	printf("Input positive number: ");
-	scanf_s("%d", &number);
-
-	if (number == 0) {
-		isWork = 0; // work = lie
-		break;
-		}
-
-		isError = (number <= 0);
-	 } while (isError);
-
-   int invers = 0;
-
-   // ...
-   printf("Result: %d", invers);
-
-  return 0;
-}
