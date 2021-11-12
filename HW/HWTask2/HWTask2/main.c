fdcb243c83533b4427a434ab57e507c1a90b57c0
Karaseva Katerina <k.karaseva2010@mail.ru>
2021-11-13 00:56:09 +0300

    Task 2 update

--- HW/HWTask2/HWTask2/main.c
+++ HW/HWTask2/HWTask2/main.c
@@ -1,8 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
 #include <time.h>
 
+
 int main() {
 
-	return 0;
+	setlocale(LC_ALL, "RUS");
+
+	int hidden, intro, attem;
+
+	hidden = 0;
+	attem = 0;
+
+	printf("Загаданно случайное число\n");
+	printf("Загаданное число находится в диапазоне от 1 до 1000\n");
+
+	srand(time(NULL));
+
+	intro = rand() % 1000 + 1;
+
+	while (hidden != intro) {
+
+		printf("Введите число: ");
+		scanf("%d", &hidden);
+
+		attem += 1;
+
+		if (hidden > intro) {
+			printf("Число больше загаданного\n");
+		}
+		if (hidden < intro) {
+			printf("Число меньше загаданного\n");
+		}
+		if (intro == hidden) {
+			printf("\nВы угадали загаданное число\n");
+			printf("Попыток совершено: %d", attem);
+			return 0;
+		}
+	}
 }
